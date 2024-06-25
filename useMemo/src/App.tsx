98620758abc6c1a430a5d75e09cc5527c58f9225
Muhammad Saad <saadstudent.cs@gmail.com>
2024-06-25 18:00:51 +0500

    Added: Basic Code

--- useMemo/src/App.tsx
+++ useMemo/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import './App.css';
+import { initialItems } from './utils';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [items] = useState(initialItems);
+
+  const selectedItems = items.find((item) => item.isSelected);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Number of Items: {count}</h1>
+        <h1>Selected Items: {selectedItems?.id}</h1>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
